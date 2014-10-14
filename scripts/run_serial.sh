6efd2310ec03cc667e09634b63bb6ef0f7599e1e
Miguel Aragao <miguelaragao91@gmail.com>
2014-10-14 20:38:37 +0100

    closing file after reading it and counting time on running script

--- scripts/run_serial.sh
+++ scripts/run_serial.sh
@@ -2,23 +2,23 @@ echo "----------------------------------"
 echo "Running CPD Project Serial Version"
 echo "----------------------------------\n\n"
 echo "-------- Start ex10.15 ---------"
-../serial/serial ../instances/ex10.15.in > ../results/out.ex10.15.out
+time ../serial/serial ../instances/ex10.15.in > ../results/out.ex10.15.out
 diff ../results/out.ex10.15.out ../instances/ex10.15.out
 echo "-------- End ex10.15 ---------\n\n"
 echo "-------- Start ex150.200 ---------"
-../serial/serial ../instances/ex150.200.in > ../results/out.ex150.200.out
+time ../serial/serial ../instances/ex150.200.in > ../results/out.ex150.200.out
 diff ../results/out.ex150.200.out ../instances/ex150.200.out
 echo "-------- End ex150.200 ---------\n\n"
 echo "-------- Start ex3k.8k ---------"
-../serial/serial ../instances/ex3k.8k.in > ../results/out.ex3k.8k.out
+time ../serial/serial ../instances/ex3k.8k.in > ../results/out.ex3k.8k.out
 diff ../results/out.ex3k.8k.out ../instances/ex3k.8k.out
 echo "-------- End ex3k.8k ---------\n\n"
 echo "-------- Start ex18k.17k ---------"
-../serial/serial ../instances/ex18k.17k.in > ../results/out.ex18k.17k.out
+time ../serial/serial ../instances/ex18k.17k.in > ../results/out.ex18k.17k.out
 diff ../results/out.ex18k.17k.out ../instances/ex18k.17k.out
 echo "-------- End ex18k.17k ---------\n\n"
 echo "-------- Start ex48k.30k ---------"
-../serial/serial ../instances/ex48k.30k.in > ../results/out.ex48k.30k.out
+time ../serial/serial ../instances/ex48k.30k.in > ../results/out.ex48k.30k.out
 diff ../results/out.ex48k.30k.out ../instances/ex48k.30k.out
 echo "-------- End ex48k.30k ---------\n\n"
 echo "----------------------------------"
--- serial/lcs_serial.c
+++ serial/lcs_serial.c
@@ -1,10 +1,8 @@
 /*
  ============================================================================
- Name        : lcs-serial.c
- Author      : André Garção
- Version     :
- Copyright   :
- Description : Sequencial implementation of the Longest Common Subsequence algoritm
+ Name        : lcs_serial.c
+ Authors     : André Garção, Miguel Aragão, Miguel Oliveira
+ Description : Sequencial implementation of the longest common subsequence algorithm
  ============================================================================
  */
 
@@ -35,11 +33,6 @@ void lcs( char *X, char *Y, int m, int n ) {
   for(i = 0; i <= m; i++) {
     L[i] = (int*) malloc(sizeof(int) * (n + 1));
   }
-  /*
-  printf("O tamanho da 1ª string é %d e da 2ª string é %d\n", m, n);
-  printf("A primeira string é: %s\n", X);
-  printf("A segunda string é: %s\n", Y);
-  */
 
   /* Following steps build L[m+1][n+1] in bottom up fashion. Note
   that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
@@ -58,19 +51,6 @@ void lcs( char *X, char *Y, int m, int n ) {
       }
     }
   }
-
-  /*
-  printf("::::::::::: LCS TABLE :::::::::::\n");
-  for(i=0; i<=m; i++)
-  {
-	  printf("|");
-	  for(j=0; j<=n; j++)
-	   {
-	   printf(" %d |", L[i][j]);
-	   }
-	  printf("\n");
-  }
-  */
 
   // Following code is used to print LCS
   int index = L[m][n];
@@ -104,7 +84,7 @@ void lcs( char *X, char *Y, int m, int n ) {
 
   /* Print the lcs */
   printf("%d\n%s\n", L[m][n], lcs);
-  /*printf("LCS of %s and %s is %s\n", X, Y, lcs);*/
+
   for(i = 0; i <= m; i++) {
     free(L[i]);
   }
@@ -142,19 +122,13 @@ int main(int argc, char *argv[]) {
 	/* Reads form the file both sequences */
 	fscanf(fp, "%s %s", firstString, secondString);
 
-  /*
-	printf("O tamanho da 1ª string é %d e da 2ª string é %d\n", firstStringSize, secondStringSize);
-	printf("A primeira string é: %s\n", firstString);
-	printf("A segunda string é: %s\n", secondString);
-	*/
-
 	/* Close the file */
-	//fclose(fp);
+	fclose(fp);
 
 	/* Calling the function that compute the LCS (In the statement says: "In order for the
 	 * solution be unique you should follow these rules: associate the first string with the
 	 * rows (and the second with the columns); when moving backwards in the matrix to determine
-	 *  the subsequence, in case up and left have the same vaue always move left." */
+	 * the subsequence, in case up and left have the same vaue always move left." */
 	lcs(secondString, firstString, secondStringSize, firstStringSize);
 
   free(secondString);
