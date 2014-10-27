9a47ceb90bbddc9e781ccac05c0f6d3fb483093d
Miguel Aragao <miguelaragao91@gmail.com>
2014-10-27 16:47:59 +0000

    new name

--- serial/lcs-serial.c
+++ serial/lcs-serial.c
@@ -40,18 +40,20 @@ void lcs( char *X, char *Y, int m, int n ) {
   start = omp_get_wtime();
   /* Following steps build L[m+1][n+1] in bottom up fashion. Note
   that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
-  for(i=0; i<=m; i++) {
-    for(j=0; j<=n; j++) {
-      if (i == 0 || j == 0) {
-    	 L[i][j] = 0;
-      }
-      else {
-    	 if (X[i-1] == Y[j-1]) {
-    	   L[i][j] = L[i-1][j-1] + cost(i);
-     	 }
-    	 else {
-    		 L[i][j] = max(L[i-1][j], L[i][j-1]);
-    	 }
+  for(i = 0; i <= m; i++) {
+    L[i][0] = 0;
+  }
+
+  for(i = 0; i <= n; i++) {
+    L[0][i] = 0;
+  }
+
+  for(i=1; i<=m; i++) {
+    for(j=1; j<=n; j++) {
+    	if (X[i-1] == Y[j-1]) {
+    	 L[i][j] = L[i-1][j-1] + cost(i);
+     	} else {
+    		L[i][j] = max(L[i-1][j], L[i][j-1]);
       }
     }
   }
